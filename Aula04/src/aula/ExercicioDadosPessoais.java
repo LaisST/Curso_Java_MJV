1c0a08665c95fb6be79fcfe8577a7c6bb50b94a1
Lais.ST <Laiscostast@gmail.com>
2021-12-20 18:47:56 -0300

    Inclusão da aula 04

---
+++ Aula04/src/aula/ExercicioDadosPessoais.java
@@ -0,0 +1,23 @@
+package aula;
+
+
+public class ExercicioDadosPessoais {
+	public static void main (String [] args ) {
+		String nome = "Lais";
+		String cpf = "39871726813";
+		int idade = 28;
+		double altura = 1.50;
+		boolean doadorOrgao = true;
+		String email = "laiscostast@gmail.com";
+		String telefone = "(11) 99485-3110";
+		String dataNascimento = "13/11/1993";
+		
+		System.out.println(nome);
+		System.out.println(cpf);
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(doadorOrgao);
+		
+	}
+		
+	}
